92d38cbe142c4942416f41a7b7a4c645afcc27fd
KAbhishekRobosoft <abhishek.kamath@robosoftin.com>
2022-12-28 13:05:32 +0530

    added splashscreen

--- FortSquare/App.js
+++ FortSquare/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import NavigationFunctionality from './src/utils/NavigationFunctionality';
 import store from './src/redux/Store';
@@ -6,9 +6,13 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistStore} from 'redux-persist';
 import ImageDisplay from './src/screens/ImageDisplay';
+import SplashScreen from 'react-native-splash-screen';
 
 let persistor = persistStore(store);
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
